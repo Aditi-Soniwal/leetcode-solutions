d666b342822f5fcdedcd64dc3092844858cc5f60
Aditi-Soniwal <aditisoniwal31@gmail.com>
2026-09-24 00:04:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.74%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        
+        if (n == 0) {
+            return 0;
+        }
+
+        int x = 0;
+        int y = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int fibo = x + y;
+            x = y;
+            y = fibo;
+        }
+
+        return y;
+    }
+}
